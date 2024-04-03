673871d5005680a9339c085bfded0d6cdd251b2a
irgiahmadm <irgiahmadmaulana@gmail.com>
2024-04-03 22:38:04 +0700

    fix: setup validation and logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { MemberController } from './member/member.controller';
 import { MemberModule } from './member/member.module';
+import { BookController } from './book/book.controller';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [BookModule, MemberModule],
-  controllers: [AppController, MemberController],
-  providers: [AppService],
+  imports: [BookModule, MemberModule, CommonModule],
+  controllers: [MemberController, BookController],
+  providers: [],
 })
 export class AppModule {}
